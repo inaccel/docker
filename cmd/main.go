3eeecd8936c91930195f94d1c78fc35d65a55d27
Elias Koromilas <elias.koromilas@gmail.com>
2021-03-26 21:46:00 +0200

    Implement Docker CLI plugin mechanism

---
+++ cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net"
+	"net/url"
+
+	"github.com/docker/cli/cli-plugins/manager"
+	"github.com/docker/cli/cli-plugins/plugin"
+	"github.com/docker/cli/cli/command"
+	"github.com/inaccel/docker/internal"
+	"github.com/inaccel/docker/internal/cmd"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var version string
+
+func main() {
+	plugin.Run(func(cli command.Cli) *cobra.Command {
+		inaccel := &cobra.Command{
+			Use:     "inaccel",
+			Short:   "Simplifying FPGA management in Docker",
+			Version: version,
+			PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+				if err := plugin.PersistentPreRunE(cmd, args); err != nil {
+					return err
+				}
+
+				viper.BindPFlag("debug", cmd.Parent().Parent().Flags().Lookup("debug"))
+				viper.BindPFlag("log-level", cmd.Parent().Parent().Flags().Lookup("log-level"))
+
+				endpoint := cli.DockerEndpoint()
+				internal.Host, _ = url.Parse(endpoint.Host)
+
+				switch internal.Host.Scheme {
+				case "unix":
+					return nil
+				default:
+					return net.UnknownNetworkError(internal.Host.Scheme)
+				}
+			},
+		}
+
+		inaccel.AddCommand(cmd.Down, cmd.Exec, cmd.Logs, cmd.Ps, cmd.Up)
+
+		return inaccel
+	}, manager.Metadata{
+		SchemaVersion: "0.1.0",
+		Vendor:        "InAccel <info@inaccel.com>",
+		Version:       version,
+		URL:           "https://inaccel.com",
+	})
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,19 @@
 module github.com/inaccel/docker
+
+go 1.15
+
+require (
+	github.com/Microsoft/hcsshim v0.8.15 // indirect
+	github.com/containerd/continuity v0.0.0-20210315143101-93e15499afd5 // indirect
+	github.com/docker/cli v20.10.5+incompatible
+	github.com/docker/docker v20.10.5+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.6.3 // indirect
+	github.com/fvbommel/sortorder v1.0.2 // indirect
+	github.com/moby/sys/mount v0.2.0 // indirect
+	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.7.0
+	github.com/theupdateframework/notary v0.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210324051608-47abb6519492 // indirect
+)
